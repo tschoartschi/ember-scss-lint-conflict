f0ca528173e8776c00a2fd94d60112678631f745
Georg Kothmeier <georg.kothmeier@roomle.com>
2017-07-28 08:15:30 +0200

    Add addon folder to scssLint otherwise files in addon are not linted

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     // Add options here
+    scssLintOptions: {
+      includePaths: [
+        'addon'
+      ]
+    }
   });
 
   /*
